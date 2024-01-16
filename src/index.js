b1b3215701cecd2dfb9c0a598cbb99280de687e5
Shubham Rajput <rajputshubham615@gmail.com>
2024-01-16 16:32:18 +0530

    Learnt How to render in the react.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import LogIn from './login';
+import Register from './Register';
 
-const root=ReactDOM.createRoot(document.getElementById('root'));
+const root =ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
-)
-root.render(
-  <h1>Hi</h1>
-)
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
+
+const register=ReactDOM.createRoot(document.getElementById('register'));
+register.render(
+  <Register />
+);
+
+const logIn=ReactDOM.createRoot(document.getElementById('logIn'));
+logIn.render(
+  <LogIn />
+);
+
+// reportWebVitals();
---
+++ src/login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LogIn(){
+    return React.createElement('h1',{},"Log in Now");
+}
+
+export default LogIn;
