be94ba29bf8d6b66695e7a5f99e9a91aefeb3f83
Shubham Rajput <rajputshubham615@gmail.com>
2024-01-18 11:00:38 +0530

    learnt conditional rendering and map function for iteration

--- src/footer.js
+++ src/footer.js
@@ -1,5 +1,37 @@
 import React from "react";
 function Footer(){
-    return <h1>Footer Section</h1>
+    var employees = [
+        { empId: 1234, name: "John", designation: "SE" , exp: 2.5},
+        { empId: 4567, name: "Tom", designation: "SSE" , exp: 5.5},
+        { empId: 8910, name: "Kevin", designation: "TA", exp: 1.5 },
+      ];
+    return (
+        <React.Fragment>
+             <h1>Footer Section</h1>
+             <table>
+                <thead>
+                    <tr>
+                        <th>EmpID</th>
+                        <th>Name</th>
+                        <th>Designation</th>
+                        <th>Eligibility</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.map((employee)=>{
+                        return(
+                            <tr key={employee.empId}>
+                                <td>{employee.empId}</td>
+                                <td>{employee.name}</td>
+                                <td>{employee.designation}</td>
+                                <td>{employee.exp>=2.5 ? <h3>Eligible</h3>:<h3>Not Eligible</h3>}</td>
+                                
+                            </tr>
+                        )
+                    })}
+                </tbody>
+             </table>
+        </React.Fragment>
+    )
 }
 export default Footer;
--- src/profile.js
+++ src/profile.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function Profile(){
-    return <h1>Profile Section</h1>
+    let element=null;
+    let isLoggedIn=false;
+    if(isLoggedIn){
+        element=<h2>Welcome Admin</h2>
+    }else{
+        element=<h3>Please Login</h3>
+    }
+    return (
+        <React.Fragment>
+            <h1>Profile Section</h1>
+            <h1>{element}</h1>
+            {isLoggedIn&& <h2>WC</h2>}
+            {!isLoggedIn&& <h2>sorry</h2>}
+        </React.Fragment>
+    )
 }
 
 export default Profile;
